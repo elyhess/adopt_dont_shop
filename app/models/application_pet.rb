7bbe68b4fe2887235d3d685172063b0eea5e3b41
Ely Hess <ely.hess@me.com>
2020-12-17 15:44:22 -0700

    Remove incomplete method

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -1,7 +1,7 @@
 class ApplicationPet < ApplicationRecord
   scope :is_approved, -> { where('status = ?', "Approved") }
   scope :is_rejected, -> { where('status = ?', "Rejected") }
-  scope :find_applications, -> (application_id) { where(application_id: application_id) }
+  scope :app_pets, -> (app_id) { where(application_id: app_id) }
 
   belongs_to :application
   belongs_to :pet
@@ -9,26 +9,12 @@ class ApplicationPet < ApplicationRecord
   validates :pet_id, uniqueness: { scope: :application_id }
   
 
-  def self.all_pets_approved?(application_id)
-    find_applications(application_id).is_approved.count == find_applications(application_id).count
+  def self.all_pets_approved?(app_id)
+    app_pets(app_id).is_approved.count == app_pets(app_id).count
   end
 
-  def self.any_pets_rejected?(application_id)
-    find_applications(application_id).is_rejected.count >= 1
+  def self.any_pets_rejected?(app_id)
+    app_pets(app_id).is_rejected.count >= 1
   end
-
-  # def self.approve_or_reject(application, status)
-  #   if status == "Approved"
-  #     update(status: "Approved")
-  #     if all_pets_approved?(application.id)
-  #       application.approval
-  #     end
-  #   elsif status == "Rejected" 
-  #     update(status: "Rejected")
-  #     if any_pets_rejected?(application.id)
-  #       application.rejection
-  #     end
-  #   end
-  # end
 
 end
