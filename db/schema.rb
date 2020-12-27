d41a04e0b2851caddeed3a158cac1fb6f5b5a336
Ely Hess <Ely.hess@me.com>
2020-12-27 12:14:03 -0700

    Add user migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_27_163129) do
+ActiveRecord::Schema.define(version: 2020_12_27_184318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2020_12_27_163129) do
     t.integer "zip_code"
     t.string "description"
     t.string "application_status", default: "In Progress"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_applications_on_user_id"
   end
 
   create_table "pets", force: :cascade do |t|
@@ -55,9 +57,11 @@ ActiveRecord::Schema.define(version: 2020_12_27_163129) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.integer "role", default: 0
   end
 
   add_foreign_key "application_pets", "applications"
   add_foreign_key "application_pets", "pets"
+  add_foreign_key "applications", "users"
   add_foreign_key "pets", "shelters"
 end
