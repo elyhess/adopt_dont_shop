18e60e52ea60cc9f280a68214d944b31849fb6bf
Ely Hess <Ely.hess@me.com>
2020-12-27 13:09:40 -0700

    Add correct_application helper

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationsController < ApplicationController
+  before_action :correct_application?, only: [:show]
+
   def show
     @application = Application.find(params[:id])
     if params[:pet_search]
@@ -11,6 +13,7 @@ class ApplicationsController < ApplicationController
 
   def create
     application = Application.new(application_params)
+    application.user_id = current_user.id
     if application.save
       redirect_to application_path(application.id)
     else 
@@ -30,7 +33,13 @@ class ApplicationsController < ApplicationController
   end
 
   private
+
   def application_params
-    params.permit(:name, :street, :city, :state, :zip_code)
+    params.permit(:name, :street, :city, :state, :zip_code, :user_id)
+  end
+
+  def correct_application?
+    @application = Application.find(params[:id])
+    render file: "/public/404" unless @application.user_id == current_user.id
   end
 end
