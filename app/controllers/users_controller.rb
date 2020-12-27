d6618e3cd490d34b11a397d73c0c9641f583f37c
Ely Hess <Ely.hess@me.com>
2020-12-27 09:32:17 -0700

    Add create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    User.create(user_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password)
+  end
 end
